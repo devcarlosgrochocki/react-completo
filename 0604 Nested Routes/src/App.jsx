bec68e7b1a0762a7de11f753502209d22b199367
Carlos Santos <carlossantosgrochocki@gmail.com>
2025-01-15 10:18:15 -0300

    atualizando nested routes

--- 0604 Nested Routes/src/App.jsx
+++ 0604 Nested Routes/src/App.jsx
@@ -3,9 +3,6 @@ import Home from './Home';
 import Sobre from './Sobre';
 import Login from './Login';
 import Produto from './Produto';
-import ProdutoDescricao from './ProdutoDescricao';
-import ProdutoAvaliacao from './ProdutoAvaliacao';
-import ProdutoCustomizado from './ProdutoCustomizado';
 import Header from './Header';
 import Pagina404 from './Pagina404';
 
@@ -17,10 +14,8 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="sobre" element={<Sobre />} />
         <Route path="login" element={<Login />} />
-        <Route path="produto/:id" element={<Produto />}>
-          <Route index element={<ProdutoDescricao />} />
-          <Route path="avaliacao" element={<ProdutoAvaliacao />} />
-          <Route path="customizado" element={<ProdutoCustomizado />} />
+        <Route path="produto">
+          <Route path=":id" element={<Produto />} />
         </Route>
         <Route path="*" element={<Pagina404 />} />
       </Routes>
--- 0604 Nested Routes/src/Produto.jsx
+++ 0604 Nested Routes/src/Produto.jsx
@@ -1,16 +1,27 @@
-import { useParams, Link, Outlet } from 'react-router-dom';
+import { useParams, Link, Outlet, Routes, Route } from 'react-router-dom';
+
+import ProdutoDescricao from './ProdutoDescricao';
+import ProdutoAvaliacao from './ProdutoAvaliacao';
+import ProdutoCustomizado from './ProdutoCustomizado';
+
 const Produto = () => {
   const params = useParams();
+
+  console.log(params.id);
 
   return (
     <div>
       <h1>Produto: {params.id}</h1>
       <nav>
-        <Link to="/">Descrição</Link>
-        <Link to="/avaliacao">Avaliação</Link>
-        <Link to="/customizado">Customizado</Link>
+        <Link to="../">Descrição</Link>
+        <Link to="../avaliacao">Avaliação</Link>
+        <Link to="../customizado">Customizado</Link>
       </nav>
-      <Outlet />
+      <Routes>
+        <Route path="/" element={<ProdutoDescricao />} />
+        <Route path="avaliacao" element={<ProdutoAvaliacao />} />
+        <Route path="customizado" element={<ProdutoCustomizado />} />
+      </Routes>
     </div>
   );
 };
